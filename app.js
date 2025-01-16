a7c0e54e069027580917e2a585199a963591a4fb
Super Dev <superdev1031@gmail.com>
2025-01-16 09:23:48 -0500

    read/write stream

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
 const http = require("http");
+const fs = require("fs");
+
+// readStream.on("data", (chunk) => {
+//   console.log("new data received");
+//   console.log(chunk);
+//   writeStream.write(chunk);
+// });
 
 const server = http.createServer((req, res) => {
-  console.log(req.url);
   res.writeHead(200, { "Content-Type": "text/plain" });
-  res.end("hello world");
+
+  const readStream = fs.createReadStream(__dirname + "/read-me.txt", "utf-8");
+  // const writeStream = fs.createWriteStream(__dirname + "/write-me.txt");
+
+  readStream.pipe(res);
+
+  // res.end("hello world");
 });
 
 console.log("Listening on port 3000");
