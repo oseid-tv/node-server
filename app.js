1fed10f95067ba9973804d5ab3f37fb50158819a
super dev <superdev1031@gmail.com>
2025-01-18 08:22:58 -0400

    create partial view

--- app.js
+++ app.js
@@ -5,15 +5,19 @@ const app = express();
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
+  res.render('index');
 });
 
 app.get('/about', (req, res) => {
-  res.sendFile(__dirname + '/about.html');
+  res.render('about');
 });
 
 app.get('/profile/:name', (req, res) => {
-  const data = { age: 30, job: 'developer' };
+  const data = {
+    age: 30,
+    job: 'developer',
+    friends: ['sarah', 'jane', 'willma'],
+  };
   res.render('profile', { person: req.params.name, data });
 });
 
